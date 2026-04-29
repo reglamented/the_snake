dabeca711fd66afb87d5881ba9c395d234aa11f7
reglamented <qqwe68839@gmail.com>
2026-04-29 18:37:21 +0300

    Added game field

--- the_snake.py
+++ the_snake.py
@@ -150,7 +150,18 @@ class Apple(GameObject):
         )
         
         pygame.draw.rect(surface, self.body_color, rect)
-        
+
+
+def draw_game_area(snake, apple, bombs):
+    """Игровое поле"""
+    screen.fill(BOARD_BACKGROUND_COLOR)
+    for segment in snake.positions:
+        snake.draw_cell(segment)
+    for bomb in bombs:
+        bomb.draw()
+
+    if apple.position is not None:
+        apple.draw()
 
 
 def main():
